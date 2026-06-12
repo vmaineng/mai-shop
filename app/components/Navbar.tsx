4cef6db837fa92e6dde766ebd90d137012a931bc
Mai Vang <vmaineng@gmail.com>
2026-06-11 17:08:50 -0700

    added in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,27 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Navbar() {
   const NAV_ITEMS = ["Home", "Shop", "Cart", "Contact"];
-  return <div>Navbar</div>;
+
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  return (
+    <nav className="w-full mx-auto px-6 sm:px-8 py-4 flex items-center justify-between">
+      <div className="bg-transparent text-md text-(--text-secondary)">
+        Shoppeee
+      </div>
+      <div className=" md:flex items-center gap-8">
+        {NAV_ITEMS.map((item) => (
+          <div
+            key={item}
+            className="relative text-sm text-(--text-secondary) transition-colors"
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </nav>
+  );
 }
